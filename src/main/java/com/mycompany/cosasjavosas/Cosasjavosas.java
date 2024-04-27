85fce9e94a5338d3a58d652b8d4d97933d18e17e
werty534 <tomas.canos@edu.uah.es>
2024-04-27 19:01:44 +0200

    creado y tal

---
+++ src/main/java/com/mycompany/cosasjavosas/Cosasjavosas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cosasjavosas;
+
+/**
+ *
+ * @author cojones
+ */
+public class Cosasjavosas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
