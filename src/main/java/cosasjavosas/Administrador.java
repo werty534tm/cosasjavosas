962d0ecbdb25f7b1da74bd3a322410483206846d
luver <luver@luver-VivoBook-15-ASUS-Laptop-X540BA>
2024-04-27 19:27:44 +0200

    ADMIN LA CHUPA

---
+++ src/main/java/cosasjavosas/Administrador.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cosasjavosas;
+
+/**
+ *
+ * @author luver
+ */
+public class Administrador {
+    
+    private String correo;
+    private String clave;
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public Administrador(String correo, String clave) {
+        this.correo = correo;
+        this.clave = clave;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrador{" + "correo=" + correo + ", clave=" + clave + '}';
+    }
+    
+}
